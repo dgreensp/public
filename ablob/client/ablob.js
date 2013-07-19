d3b09eddf989320ae647729c61ff6f9e88fb2dcb
David Greenspan <dgreenspan@alum.mit.edu>
2013-07-18 21:59:53 -0700

    gonna use meteor ui after all

--- ablob/client/ablob.js
+++ ablob/client/ablob.js
@@ -1,8 +1,35 @@
 
 
+var blobLoc = [0, 0];
+var blobElem;
+
 Meteor.startup(function () {
 
-  $('body').append('<div id="wrapper"><div id="container"><div id="board"></div></div></div>');
+  blobElem = $('.block').get(0);
+  moveBlob(0, 0);
 
-  $('#board').append('<div class="block">');
+  $(document).keydown(function (evt) {
+    switch (evt.which) {
+    case 37: // left
+      moveBlob(-1,0);
+      break;
+    case 38: // up
+      moveBlob(0,-1);
+      break;
+    case 39: // right
+      moveBlob(1,0);
+      break;
+    case 40: // down
+      moveBlob(0,1);
+      break;
+    }
+  });
 });
+
+moveBlob = function (dx, dy) {
+  var x = (blobLoc[0] += dx);
+  var y = (blobLoc[1] += dy);
+
+  $(blobElem).css({left: x*40 - 20,
+                   top: y*40 - 20});
+};
