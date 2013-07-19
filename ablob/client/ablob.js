86b84ff106196276af5cd1516b499d07a021ca8c
David Greenspan <dgreenspan@alum.mit.edu>
2013-07-18 20:53:42 -0700

    ablob

---
+++ ablob/client/ablob.js
@@ -0,0 +1,8 @@
+
+
+Meteor.startup(function () {
+
+  $('body').append('<div id="wrapper"><div id="container"><div id="board"></div></div></div>');
+
+  $('#board').append('<div class="block">');
+});
