bc608b88b693cecf8d82d61eea7b421d7cca289c
David Greenspan <dgreenspan@alum.mit.edu>
2015-11-25 11:53:03 -0800

    Asymptotic improvements for Multibuffer slicing

--- app4/src/Multibuffer.js
+++ app4/src/Multibuffer.js
@@ -5,6 +5,13 @@ export class Multibuffer {
   constructor(chunks, _totalLength = calcTotalLength(chunks)) {
     this.chunks = chunks;
     this.length = _totalLength;
+
+    let count = 0;
+    this.chunkStarts = this.chunks.map(ch => {
+      const start = count;
+      count += ch.length;
+      return start;
+    });
   }
 
   toBuffer() {
@@ -22,23 +29,45 @@ export class Multibuffer {
       end = start;
     }
     const newChunks = [];
-    let offset = 0;
-    // This implementation is O(N) in the number of chunks,
-    // whereas it could be O(log N) using binary search,
-    // at the cost of complexity and with not much speed-up for
-    // very small N.
-    for (let ch of this.chunks) {
-      const startInChunk = Math.max(start - offset, 0);
-      const endInChunk = Math.min(end - offset, ch.length);
-      if (startInChunk === 0 && endInChunk === ch.length) {
-        newChunks.push(ch);
-      } else if (endInChunk > startInChunk) {
-        newChunks.push(ch.slice(startInChunk, endInChunk));
+    // `firstChunk` is index of first chunk whose end is past `start`
+    const firstChunk = binarySearch(this.chunks.length, k => {
+      return this.chunkStarts[k] + this.chunks[k].length > start;
+    });
+    if (firstChunk < this.chunks.length) {
+      let offset = this.chunkStarts[firstChunk];
+      for (let i = firstChunk; i < this.chunks.length; i++) {
+        if (offset >= end) {
+          break;
+        }
+        const ch = this.chunks[i];
+        if (ch.length) {
+          const startInChunk = Math.max(start - offset, 0);
+          const endInChunk = Math.min(end - offset, ch.length);
+          if (startInChunk === 0 && endInChunk === ch.length) {
+            newChunks.push(ch);
+          } else if (endInChunk > startInChunk) {
+            newChunks.push(ch.slice(startInChunk, endInChunk));
+          }
+          offset += ch.length;
+        }
       }
-      offset += ch.length;
     }
     return new Multibuffer(newChunks, end - start);
   }
+}
+
+// requires N > 0
+function binarySearch(N, func) {
+  let a = 0;
+  let b = N-1;
+  if (func(a)) return 0;
+  if (!func(b)) return N;
+  while (b-a > 1) {
+    const c = Math.floor((a+b)/2);
+    if (func(c)) b = c;
+    else a = c;
+  }
+  return b;
 }
 
 function calcTotalLength(chunks) {
