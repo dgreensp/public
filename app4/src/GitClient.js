e2e404fb0de60fb47e81ece4d6cadeaf88ed4b16
David Greenspan <dgreenspan@alum.mit.edu>
2015-11-25 12:06:27 -0800

    Try fetching from other repos

--- app4/src/GitClient.js
+++ app4/src/GitClient.js
@@ -7,17 +7,22 @@ export function getDiscovery() {
   });
 }
 
+const SHAS = {
+  meteor: '9740c232c1b0c4a6358651bc632a21e48769136d',
+  public: 'bc608b88b693cecf8d82d61eea7b421d7cca289c'
+};
+
 export function getPack() {
-  const nocap = `0032want 9740c232c1b0c4a6358651bc632a21e48769136d
-0032want 9740c232c1b0c4a6358651bc632a21e48769136d
+  const nocap = `0032want ${SHAS.public}
+0032want ${SHAS.public}
 00000009done
 `;
-  const allcap = `007fwant 9740c232c1b0c4a6358651bc632a21e48769136d multi_ack_detailed no-done side-band-64k thin-pack ofs-delta agent=git/1.9.2
-0032want 9740c232c1b0c4a6358651bc632a21e48769136d
+  const allcap = `007fwant ${SHAS.public} multi_ack_detailed no-done side-band-64k thin-pack ofs-delta agent=git/1.9.2
+0032want ${SHAS.public}
 00000009done
 `;
   return request({
-    uri: 'https://github.com/meteor/meteor.git/git-upload-pack',
+    uri: 'https://github.com/dgreensp/public.git/git-upload-pack',
     method: 'POST',
     body: nocap,
     headers: {
