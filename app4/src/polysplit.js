ac668d3fdbe9d43fe2c1aed54a7a9d64b725e4d5
David Greenspan <dgreenspan@alum.mit.edu>
2015-11-27 21:42:18 -0800

    Polysplit improvements, interners
    untested and not looking great right now

--- app4/src/polysplit.js
+++ app4/src/polysplit.js
@@ -1,25 +1,85 @@
-const N = 199;
-const A = N*N*N;
-const B = N*N;
-const C = N;
+const P = 199;
+const A = P*P*P;
+const B = P*P;
+const C = P;
 
-export function polysplit(data, a) {
+global.HITS = 0;
+global.MISSES = 0;
+global.CHAIN_ADVANCES = 0;
+
+export function polysplit(data, N, maxLen=N*2, minLen=4) {
   if (data.length <= 4) {
     return [data];
   }
   const chunks = [];
-  let h = (A * data.readUInt8(0) +
-           B * data.readUInt8(1) +
-           C * data.readUInt8(2) +
-           data.readUInt8(3)) >>> 0;
+  let a = data.readUInt8(0);
+  let b = data.readUInt8(1);
+  let c = data.readUInt8(2);
+  let d = data.readUInt8(3);
+  let h = (A*a + B*b + C*c + d) >>> 0;
   let chunkStart = 0;
-  for (let i = 1; i+4 <= data.length; i++) {
-    h = ((h - A * data.readUInt8(i-1)) * N + data.readUInt8(i+3)) >>> 0;
-    if ((i - chunkStart) >= 4 && (h % a === 1)) {
+  let chunkSize = 1; // (i - chunkStart)
+  for (let i = 1; i+4 <= data.length; i++,chunkSize++) {
+    let oldA = a;
+    a = b;
+    b = c;
+    c = d;
+    d = data.readUInt8(i+3);
+    h = ((h - A * oldA) * P + d) >>> 0;
+    if (chunkSize >= minLen &&
+        ((h % N === 1) || chunkSize >= maxLen)) {
       chunks.push(data.slice(chunkStart, i));
       chunkStart = i;
+      chunkSize = 0;
     }
   }
   chunks.push(data.slice(chunkStart));
   return chunks;
+}
+
+// Idea taken from https://github.com/darkskyapp/string-hash
+export function fasthash(buffer) {
+  // the constants here are chosen totally at random
+  let hash = 0x72677172;
+  let i = buffer.length;
+  while (i) {
+    hash = (hash * 0x3c03e4d1) ^ buffer.readUInt8(--i);
+  }
+
+  return (hash << 7) >>> 0;
+}
+
+export class BufferInterner {
+  constructor() {
+    this.hashmap = {};
+  }
+
+  intern(buffer) {
+    let h = fasthash(buffer);
+    while (this.hashmap[h] && ! this.hashmap[h].equals(buffer)) {
+      global.CHAIN_ADVANCES++;
+      h = (h + 1) >>> 0;
+    }
+    if (! this.hashmap[h]) {
+      global.MISSES++;
+      // copy, so we aren't storing slices of larger Buffers!
+      this.hashmap[h] = new Buffer(buffer);
+    } else global.HITS++;
+    return this.hashmap[h];
+  }
+}
+
+export class PolysplitInterner {
+  constructor(N, maxLen=undefined, minLen=undefined) {
+    this.N = N;
+    this.maxLen = maxLen;
+    this.minLen = minLen;
+    this.interner = new BufferInterner();
+  }
+
+  intern(buffer) {
+    const {interner, N, maxLen, minLen} = this;
+    return polysplit(buffer, N, maxLen, minLen).map(
+      chunk => interner.intern(chunk));
+  }
 }
